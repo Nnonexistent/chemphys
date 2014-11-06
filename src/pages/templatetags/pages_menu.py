a7ff8e59d933b77d04cc2547a26eb0ef13f9aab5
Nik <nnonexistent@gmail.com>
2014-11-06 13:41:55 +0300

    adds pages_menu templatetags for pages app, fixed template path for page.html

---
+++ src/pages/templatetags/pages_menu.py
@@ -0,0 +1,18 @@
+from django import template
+
+from pages.models import Page
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def pages_menu(context):
+    out = []
+    for page in Page.objects.filter(in_menu=True):
+        out.append(u'<li%s><a href="%s">%s</a></li>' % (
+            ' class="active"' if context.get('page') == page else '',
+            page.get_absolute_url(),
+            page.title
+        ))
+    return u'\n'.join(out)
