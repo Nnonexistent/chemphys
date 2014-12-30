d56f05c5180803f64ab17b8765405945b28f93eb
Nik <nnonexistent@gmail.com>
2014-12-30 07:25:32 +0300

    loc_user context processor and article add fix

--- src/journal/context_processors.py
+++ src/journal/context_processors.py
@@ -1,11 +1,11 @@
 from journal.models import LocalizedUser
 
 def user(request):
-    if hasattr(request, 'user'):
+    if hasattr(request, 'user') and request.user.is_authenticated():
         loc_user = LocalizedUser.objects.get(id=request.user.id)
     else:
         from django.contrib.auth.models import AnonymousUser
-        user = AnonymousUser()
+        loc_user = AnonymousUser()
 
     return {
         'loc_user': loc_user,
